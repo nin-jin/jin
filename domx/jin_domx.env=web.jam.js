9617a8e06afb21fdd9fa0034dc708903f0144da5
Дмитрий Карловский <dkarlovskiy@crystals.ru>
2013-03-14 11:31:16 +0400

    пачка мелочей

--- domx/jin_domx.env=web.jam.js
+++ domx/jin_domx.env=web.jam.js
@@ -3,7 +3,12 @@ $jin_class( function( $jin_domx, domx ){
     
     $jin_wrapper.scheme( $jin_domx )
     
-    domx.toDOMDocument=
+    domx.toDOMDoc=
+    function( domx ){
+        return domx.$.ownerDocument || domx.$
+    }
+    
+    domx.toDOMNode=
     function( domx ){
         return domx.$
     }
@@ -35,26 +40,119 @@ $jin_class( function( $jin_domx, domx ){
         
         if( $jin_support.xmlModel() === 'w3c' ){
             var proc= new XSLTProcessor
-            proc.importStylesheet( domx.$ )
-            var doc= proc.transformToDocument( from )
-            to.innerHTML= $jin_domx( doc )
+            proc.importStylesheet( domx.toDOMDoc() )
+            var res= proc.transformToFragment( from, to.ownerDocument )
+            to.innerHTML= ''
+            to.appendChild( res )
         } else {
-            to.innerHTML= from.transformNode( domx.$ )
+            to.innerHTML= from.transformNode( domx.toDOMDoc() )
         }
         
         return domx
+    }
+    
+    domx.name= function( domx ){
+        return domx.$.nodeName
+    }
+    
+    domx.attr= function( domx, name, value ){
+        if( arguments.length > 2 ){
+            if( value == null ) domx.$.removeAttribute( name )
+            else domx.$.setAttribute( name, value )
+            return domx
+        } else {
+            return domx.$.getAttribute( name )
+        }
+    }
+    
+    domx.text= function( domx, value ){
+        if( arguments.length > 1 ){
+            domx.clear()
+            if( value != '' ) domx.Text( value ).parent( domx )
+            return domx
+        } else {
+            if( $jin_support.xmlModel() === 'w3c' ){
+                return domx.$.textContent
+            } else {
+                return domx.$.text
+            }
+        }
+    }
+    
+    domx.clear= function( domx ){
+        var child
+        while( child= domx.$.firstChild ){
+            domx.$.removeChild( child )
+        }
+        return domx
+    }
+    
+    domx.parent= function( domx, parent ){
+        if( arguments.length > 1 ){
+            if( parent == null ){
+                parent= domx.$.parentNode
+                if( parent ) parent.removeChild( domx.$ )
+            } else {
+                $jin_unwrap( parent ).appendChild( domx.$ )
+            }
+            return domx
+        } else {
+            parent= domx.$.parentNode
+            return parent ? $jin_domx( parent ) : parent
+        }
+    }
+    
+    domx.childList= function( domx ){
+        var nodes= domx.$.childNodes
+        var list= []
+        for( var i= 0; i < nodes.length; ++i ){
+            lst.push( $jin_domx( nodes[ i ] ) )
+        }
+        return list
+    }
+    
+    domx.select= function( domx, xpath ){
+        var list= []
+        
+        if( $jin_support.xmlModel() === 'w3c' ){
+            var found= domx.toDOMDoc().evaluate( xpath, domx.$, null, null, null )
+            for( var node; node= found.iterateNext(); ) list.push( $jin_domx( node ) )
+        } else {
+            var found= domx.$.selectNodes( xpath )
+            console.log( found.length )
+            for( var i= 0; i < found.length; ++i ) list.push( $jin_domx( found[ i ] ) )
+        }
+        
+        return list
+    }
+
+    domx.Text= function( domx, value ){
+        return $jin_domx( domx.toDOMDoc().createTextNode( value ) )
+    }
+    
+    domx.Fragment= function( domx ){
+        return $jin_domx( domx.toDOMDoc().createDocumentFragment() )
+    }
+    
+    domx.Element= function( domx, name, ns ){
+        var doc= domx.toDOMDoc()
+        if( arguments.length > 2 ){
+            return $jin_domx( doc.createElementNS( ns, name ) )
+        } else {
+            return $jin_domx( doc.createElement( name ) )
+        }
     }
     
     $jin_domx.parse= function( str ){
         if( $jin_support.xmlModel() === 'w3c' ){
             var parser= new DOMParser
             var doc= parser.parseFromString( str, 'text/xml' )
-            return $jin_domx( doc )
+            return $jin_domx( doc.documentElement )
         } else {
             var doc= new ActiveXObject( 'MSXML2.DOMDocument' )
             doc.async= false
             doc.loadXML( str )
-            return $jin_domx( doc )
+            return $jin_domx( doc.documentElement )
         }
     }
 
